11cf27d7a3088afdca0a4de703822356dfe13560
runpixelrun <justindavidson23@gmail.com>
2015-09-08 09:31:34 +0000

    Added mailer and view

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 class Contact < ActiveRecord::Base
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, :email, presence: true
+    after_create :send_email
+  
+    private
+        def send_email
+        ContactMailer.contact_email(self).deliver
+        end
 end
